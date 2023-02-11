d0b908223a18b0ad2e39fca70375cb3233c6e3f1
Saurav CV <saurav.cv@juspay.in>
2023-02-11 23:23:16 +0530

    feat:H1016-L2 - Payment authorize and sync for forte

--- crates/router/src/connector/forte/transformers.rs
+++ crates/router/src/connector/forte/transformers.rs
@@ -128,13 +128,32 @@ pub struct FortePaymentsResponse {
         response: Response
       }
 
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct DefaultResponse {
+    response_type : String,
+    response_code:String,
+    response_desc:String,
+}
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct ForteResponse {
+    transaction_id: String,
+    response: DefaultResponse
+}
+
 impl<F,T> TryFrom<types::ResponseRouterData<F, FortePaymentsResponse, T, types::PaymentsResponseData>> for types::RouterData<F, T, types::PaymentsResponseData> {
     type Error = error_stack::Report<errors::ParsingError>;
     fn try_from(item: types::ResponseRouterData<F, FortePaymentsResponse, T, types::PaymentsResponseData>) -> Result<Self,Self::Error> {
+        let mut txn_id= String::from(item.response.transaction_id);
+        txn_id.push_str(":_:");
+        txn_id.push_str(item.response.response.authorization_code.as_str());
+        txn_id.push_str(":_:");
+        txn_id.push_str(item.response.entered_by.as_str());
         Ok(Self {
             status: enums::AttemptStatus::from(enums::AttemptStatus::Authorized),
             response: Ok(types::PaymentsResponseData::TransactionResponse {
-                resource_id: types::ResponseId::ConnectorTransactionId("1234".to_string()),
+                resource_id: types::ResponseId::ConnectorTransactionId(txn_id),
                 redirection_data: None,
                 redirect: false,
                 mandate_reference: None,
@@ -144,6 +163,83 @@ impl<F,T> TryFrom<types::ResponseRouterData<F, FortePaymentsResponse, T, types::
         })
     }
 }
+
+impl<F,T> TryFrom<types::ResponseRouterData<F, ForteResponse, T, types::PaymentsResponseData>> for types::RouterData<F, T, types::PaymentsResponseData> {
+    type Error = error_stack::Report<errors::ParsingError>;
+    fn try_from(item: types::ResponseRouterData<F, ForteResponse, T, types::PaymentsResponseData>) -> Result<Self,Self::Error> {
+        Ok(Self {
+            status: enums::AttemptStatus::from(enums::AttemptStatus::Authorized),
+            response: Ok(types::PaymentsResponseData::TransactionResponse {
+                resource_id: types::ResponseId::ConnectorTransactionId(item.response.transaction_id),
+                redirection_data: None,
+                redirect: false,
+                mandate_reference: None,
+                connector_metadata: None,
+            }),
+            ..item.data
+        })
+    }
+}
+
+#[derive(Default, Debug, Clone, Serialize, PartialEq)]
+pub struct FortePaymentsCaptureRequest {
+    action: String,
+    transaction_id: String,
+    authorization_code : String
+}
+
+impl TryFrom<&types::PaymentsCaptureRouterData> for FortePaymentsCaptureRequest {
+    type Error = error_stack::Report<errors::ConnectorError>;
+    fn try_from(item: &types::PaymentsCaptureRouterData) -> Result<Self, Self::Error> {
+        let v :Vec<&str> = item.request.connector_transaction_id.as_str().split(":_:").collect();
+        let pp = Self {
+            action: "capture".to_string(),
+            transaction_id: v[0].to_string(),
+            authorization_code: v[1].to_string(),
+        };
+        println!("something 4--> {pp:?}");
+        let tmp = serde_json::to_string(&pp);
+        println!("something 5 --> {tmp:?}");
+    Ok(pp)
+    }
+}
+
+
+
+
+
+
+#[derive(Default, Debug, Clone, Serialize, PartialEq)]
+pub struct FortePaymentsVoidRequest {
+    action: String,
+    authorization_code: String,
+    entered_by: String
+}
+
+impl TryFrom<&types::PaymentsCancelRouterData> for FortePaymentsVoidRequest {
+    type Error = error_stack::Report<errors::ConnectorError>;
+    fn try_from(item: &types::PaymentsCancelRouterData) -> Result<Self, Self::Error> {
+        let v :Vec<&str> = item.request.connector_transaction_id.as_str().split(":_:").collect();
+        let pp = Self {
+            action: "void".to_string(),
+            authorization_code: v[1].to_string(),
+            entered_by: v[2].to_string()
+        };
+        println!("something 4--> {pp:?}");
+        let tmp = serde_json::to_string(&pp);
+        println!("something 5 --> {tmp:?}");
+    Ok(pp)
+    }
+}
+
+
+
+
+
+
+
+
+
 
 //TODO: Fill the struct with respective fields
 // REFUND :
